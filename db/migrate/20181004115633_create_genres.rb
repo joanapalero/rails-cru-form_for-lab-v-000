74c00b5e7f1360957277817d03aa579a9d18d68a
Joana Palero <joana.palero24@gmail.com>
2018-10-04 11:56:40 +0000

    automatically backed up by learn

--- db/migrate/20181004115633_create_genres.rb
+++ db/migrate/20181004115633_create_genres.rb
@@ -2,7 +2,7 @@ class CreateGenres < ActiveRecord::Migration
   def change
     create_table :genres do |t|
       t.string :name
-      t.timestamps :false
+      t.timestamps null:false
     end
   end
 end

d2fa88ef9712a8563d10173669dc87a569dfdcfd
Joana Palero <joana.palero24@gmail.com>
2018-10-04 11:55:50 +0000

    automatically backed up by learn

--- db/migrate/20181004115517_create_genres.rb
+++
@@ -1,9 +0,0 @@
-class CreateGenres < ActiveRecord::Migration
-  def change
-    create_table :genres do |t|
-      t.string :name
-      t.timestamps :false
-      t.string :null
-    end
-  end
-end
